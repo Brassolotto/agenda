b5d14253c5b63d4f6ee75d0eac5d2b4ba5b81a20
Ricardo Brassolotto <23336979+Brassolotto@users.noreply.github.com>
2025-01-31 15:19:06 -0300

    Primeiro commit

---
+++ agenda.py
@@ -0,0 +1,14 @@
+def adicionar_contatos():
+
+    contatos = []
+    contato = {
+        "nome": "Rick",
+        "telefone": "11976004453"
+    }
+
+def listar_contatos():
+
+def buscar_contato():
+
+
+def main():
